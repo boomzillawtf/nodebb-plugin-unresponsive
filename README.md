16614ac55937d880ae469d7513007a8a45c825d9
boomzillawtf <boomzillawtf@gmail.com>
2016-05-13 17:11:31 -0400

    - Settings dialog.  - Set max width and left margin when in desktop mode  - Set max post width  - Make the notifications dropdown bigger.

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ in the UI when using NodeBB. The user's preference is stored using the browser's
 the device being used to access NodeBB.
 
 ##Changes
+    0.3.0
+     - Settings dialog.
+     - Set max width and left margin when in desktop mode
+     - Set max post width
+     - Make the notifications dropdown bigger.
     0.2.0
      - Disable when the user selects a non-default skin, due to cross origin policies.
     0.1.2
--- static/unresponsive.js
+++ static/unresponsive.js
@@ -1,69 +1,264 @@
+;(function(){
 "use strict";
 
-var nodebbTranslator;
+var nodebbTranslator,
+	rawStrings = [
+		['unresponsive','responsive'],
+		['unresponsive','desktop'],
+		['unresponsive','settings_header'],
+		['unresponsive','settings_text'],
+		['unresponsive','settings'],
+		['unresponsive','mode_header'],
+		['unresponsive','mode_text'],
+		['unresponsive','other_settings'],
+		['unresponsive','other_text'],
+		['unresponsive','cancel'],
+		['unresponsive','max_fluid_width'],
+		['unresponsive','max_content_width'],
+		['unresponsive','max_notification_height'],
+		['unresponsive','left_margin'],
+		['global','save_changes']
+	],
+	strings = { translated: 0 },
+	settings,
+	hasCustomStyle,
+	settingsDiv,
+	modalDiv;
+
 var unresponsive = function(translator){
-	nodebbTranslator = translator;
-}
-function beResponsive(){
-	window.localStorage['responsive'] = 'responsive';
-	location.reload();
-}
-
-function beUnresponsive(){
-	window.localStorage['responsive'] = 'unresponsive';
-	location.reload();
-}
-
-function createMenuItem(text, selected){
-	var li = document.createElement('li');
-	var a = document.createElement('a');
-	li.appendChild(a);
-	a.href = '#';
-	var i = document.createElement('i');
-	a.appendChild(i);
-	i.className = 'fa fa-fw';
-	if( selected ){
-		i.classList.add('fa-dot-circle-o');
-	}
-	else{
-		i.classList.add('fa-circle-o');
-	}
-	var span = document.createElement('span');
-	$(span).text(text);
-	a.appendChild( span );
-	return li;
-}
-
-function createMenu( menu, responsive ){
-	var divider = document.createElement("li");
-	divider.role = 'presentation';
-	divider.classList.add('divider');
-	
-	nodebbTranslator.translate('[[unresponsive:responsive]]', function(text){
-		var responsiveMenu = createMenuItem(text, responsive);
-		responsiveMenu.id = 'media-override-responsive';
-		responsiveMenu.addEventListener('click', beResponsive);
-		
-		nodebbTranslator.translate('[[unresponsive:desktop]]', function(text){
-			var unresponsiveMenu = createMenuItem( text, !responsive);
-			unresponsiveMenu.addEventListener('click', beUnresponsive);
-			unresponsiveMenu.id = 'media-override-unresponsive';
-			
-			menu.appendChild( divider, responsiveMenu );
-			menu.appendChild( responsiveMenu );
-			menu.appendChild( unresponsiveMenu );
+	rawStrings.map( function( raw, ix ){
+		translator.translate( '[[' + raw[0] + ':' + raw[1] + ']]', function( text ){
+			strings[raw[1]] = text;
+			++strings.translated;
+			if( strings.translated == rawStrings.length ){
+				nodebbTranslator = translator;
+			}
 		});
 	});
 }
 
-function addResponsiveMenu(responsive){
+function beResponsive(){
+	settings.responsive = true;
+	saveSettings()
+	location.reload();
+}
+
+function beUnresponsive(){
+	settings.responsive = false;
+	saveSettings();
+	window.localStorage['responsive'] = 'unresponsive';
+	location.reload();
+}
+
+function createMenuItem( text, fa_icon){
+	var li = element(null, 'li');
+	var a = element(li, 'a');
+	a.href = '#';
+	var i = element(a, 'i',['fa','fa-fw'] );
+	if( fa_icon === true ){
+		i.classList.add( 'fa-dot-circle-o' );
+	}
+	else if (fa_icon === false ){
+		i.classList.add( 'fa-circle-o' );
+	}
+	else if( fa_icon ){
+		i.classList.add( fa_icon );
+	}
+	var span = element(a, 'span', [], text);
+	return li;
+}
+
+function element(parent, elementName, classes, text){
+	var el = document.createElement(elementName);
+	if(parent){
+		parent.appendChild(el);
+	}
+	if( classes ){
+		if( !Array.isArray(classes) ){
+			classes = [classes];
+		}
+		for( var i = 0; i < classes.length; ++i ){
+			el.classList.add(classes[i]);
+		}
+	}
+	$(el).text(text);
+	return el;
+}
+
+function radio(parent, name, value){
+	var r = element(parent, "input", ["unresponsive"]);
+	r.type = "radio";
+	r.name = name;
+	r.value = value;
+	return r;
+}
+
+function input(parent, id, value){
+	var i = element(parent, "input", "form-control");
+	i.id = id;
+	i.type = "text";
+	i.value = value;
+	return i;
+}
+
+
+function label(parent, forElement, value ){
+	var l = element(parent, "label", null, value );
+	l.setAttribute('for', forElement);
+	return l;
+}
+
+function createSettingsDiv(){
+	var mainDiv = $("#unresponsive-setting-dialog");
+	if( mainDiv.length > 0 ){
+		return mainDiv[0];
+	}
+	
+	mainDiv = element($('body')[0], "div", ["account","unresponsive-dialog"]);
+	mainDiv.id = '#unresponsive-setting-dialog';
+	modalDiv = element($('body')[0], "div", "unresponsive-modal");
+	modalDiv.addEventListener("click", function(){$(modalDiv).hide(); $(settingsDiv).hide();});
+	
+	element(mainDiv, "h2", [], strings["settings_header"]);
+	element(mainDiv, "span", [], strings["settings_text"]);
+	
+	var form = element(mainDiv, "form");
+	var row = element(form, "div", "row");
+	
+	element( row, "h4", [], strings["mode_header"]);
+	var modeWell = element( form, "div", "well");
+	var responsiveRadio = radio(modeWell, "unresponsive_mode", "responsive");
+	responsiveRadio.id = 'responsive-radio';
+	label( modeWell, "responsive-radio", strings["responsive"] );
+	element( modeWell, "br");
+	
+	var unresponsiveRadio = radio(modeWell, "unresponsive_mode", "unresponsive");
+	unresponsiveRadio.id = 'unresponsive-radio';
+	label( modeWell, 'unresponsive-radio', strings["desktop"] );
+	element( modeWell, "br");
+	
+	label( modeWell, "max-fluid-width", strings['max_fluid_width']);
+	var maxFluidWidth = input(modeWell, "max-fluid-width", settings.maxFluidWidth ? settings.maxFluidWidth : '' );
+	element( modeWell, "br");
+	
+	label( modeWell, "left-margin", strings['left_margin']);
+	var maxFluidWidth = input(modeWell, "left-margin", settings.leftMargin ? settings.leftMargin : 'auto' );
+	element( modeWell, "br");
+	
+	element( modeWell, "span", [], strings["mode_text"]);
+	
+	var row = element(form, "div", "row");
+	element( row, "h4", [], strings["other_settings"]);
+	var otherWell = element( form, "div", "well");
+	element( otherWell, "span", [], strings["other_text"]);
+	element( otherWell, "br");
+	label( otherWell, "max-content-width", strings['max_content_width']);
+	var maxContentWidth = input(otherWell, "max-content-width", settings.maxContentWidth ? settings.maxContentWidth : '');
+	element( otherWell, "br");
+	
+	label( otherWell, "max-notification-height", strings['max_notification_height']);
+	var maxContentWidth = input(otherWell, "max-notification-height", settings.maxNotificationHeight ? settings.maxNotificationHeight : '');
+	element( modeWell, "br");
+	
+	var actionsDiv = element(form, "div", "form-actions");
+	var submit = element( actionsDiv, "a", ["btn","btn-primary"], strings["save_changes"] );
+	var cancel = element( actionsDiv, "a", "btn", strings["cancel"] );
+	submit.addEventListener('click', function(event){ onSaveSettings([mainDiv,modalDiv]);});
+	cancel.addEventListener('click', function(event){ onCancelSettings([mainDiv,modalDiv]);});
+	
+	return mainDiv;
+}
+
+function onCancelSettings( div ){
+	$(div).hide();
+}
+
+function onSaveSettings( div ){
+	var originalResponsive = settings.responsive;
+	settings.responsive = $("input[value=responsive]").prop("checked");
+	
+	var originalMaxFluid = settings.maxFluidWidth;
+	settings.maxFluidWidth = $("#max-fluid-width")[0].value;
+	
+	var originalMaxContent = settings.maxContentWidth;
+	settings.maxContentWidth = $("#max-content-width")[0].value;
+	
+	var originalMaxNotification = settings.maxNotificationHeight;
+	settings.maxNotificationHeight = $("#max-notification-height")[0].value;
+	
+	var originalLeftMargin = settings.leftMargin;
+	settings.leftMargin = $("#left-margin")[0].value;
+	
+	$(div).hide();
+	saveSettings();
+	var needsRefresh = false;
+	if( originalMaxContent != settings.maxContentWidth || originalMaxNotification != settings.maxNotificationHeight){
+		needsRefresh = true;
+	}
+	if( originalResponsive != settings.responsive ||
+		originalMaxFluid != settings.maxFluidWidth ||
+		originalLeftMargin != settings.leftMargin){
+		
+		if( settings.responsive ){
+			needsRefresh = false;
+			beResponsive();
+		}
+		else{
+			needsRefresh = false;
+			beUnresponsive();
+		}
+	}
+	if( needsRefresh ){
+		location.reload();
+	}
+}
+
+function onShowSettings(){
+	$("input[value=responsive]").prop("checked", settings.responsive );
+	$("input[value=unresponsive]").prop("checked", !settings.responsive );
+	$("#max-fluid-width")[0].value = settings.maxFluidWidth ? settings.maxFluidWidth : '';
+	$(modalDiv).show();
+	$(settingsDiv).show();
+	
+}
+
+function createMenu( menu, hasCustomStyle ){
+	var divider = element(menu, "li", "divider");
+	divider.role = 'presentation';
+	
+	var responsiveMenu = createMenuItem( strings.responsive, settings.responsive);
+	responsiveMenu.id = 'media-override-responsive';
+	responsiveMenu.addEventListener('click', beResponsive);
+	
+	var unresponsiveMenu = createMenuItem( strings.desktop, !settings.responsive);
+	unresponsiveMenu.addEventListener('click', beUnresponsive);
+	unresponsiveMenu.id = 'media-override-unresponsive';
+	
+	menu.appendChild( divider, responsiveMenu );
+	if( !hasCustomStyle ){
+		menu.appendChild( responsiveMenu );
+		menu.appendChild( unresponsiveMenu );
+	}
+	
+	var settingsMenu = createMenuItem( strings.settings, "fa-gear" );
+	settingsMenu.id = 'unresponsive-settings';
+	menu.appendChild( settingsMenu );
+	settingsMenu.addEventListener('click', onShowSettings );
+}
+
+function addResponsiveMenu(){
 	var userControlList = $("#user-control-list");
 	var mobileMenu = $('[component="header/usercontrol"]');
 	var existingMenu = $("#media-override-responsive");
 	if( userControlList.length > 0 && !existingMenu.length){
-		createMenu( userControlList[0], responsive );
-		createMenu( mobileMenu[0], responsive );
+		createMenu( userControlList[0], hasCustomStyle );
+		createMenu( mobileMenu[0], hasCustomStyle );
 	}
+}
+
+function initInterface(){
+	addResponsiveMenu();
+	settingsDiv = createSettingsDiv();
 }
 
 var maxWidth, minWidth, translate;
@@ -85,21 +280,40 @@ translate = {
 	'992px': '1px'
 }
 
+function styleTweaks(){
+	var s = [].slice.call(document.styleSheets);
+	if( settings.maxContentWidth ){
+		s[0].insertRule("div .content { max-width: " + settings.maxContentWidth + ";}", s[0].cssRules.length);
+	}
+	if( settings.maxNotificationHeight ){
+		s[0].insertRule(
+			"#menu .notification-list, .header .notification-list{max-height: " + settings.maxContentWidth + ";}",
+			s[0].cssRules.length);
+	}
+	
+}
+
 function forceUnresponsiveStyle(){
 	$('.container').addClass('container-fluid');
 	$('.container').removeClass('container');
 	// Get all stylesheets for ths docuemnt
 	var s = [].slice.call(document.styleSheets);
+	if( settings.maxFluidWidth ){
+		s[0].insertRule(".container-fluid { max-width: " + settings.maxFluidWidth + ";}", s[0].cssRules.length);
+	}
+	if( settings.leftMargin ){
+		s[0].insertRule(".container-fluid { margin-left: " + settings.leftMargin + ";}", s[0].cssRules.length);
+	}
 	// Iterate through each one
 	for(var ixs = 0; ixs < s.length; ixs++){
-		sheet = s[ixs];
+		var sheet = s[ixs];
 		if(sheet!=null && sheet.cssRules != null && sheet.cssRules.length > 0){
 			// Get all cssRules for this stylesheet
-			ss = sheet;
 			var r = [].slice.call(sheet.cssRules);
 			// iterate through each rule
 			for (var ixr = 0; ixr < r.length; ixr++){
 				var rule = r[ixr];
+				
 				// If this rule has a Media query, we're interested in it
 				if(rule != null && rule.media != null && rule.media && rule.media.length > 0){
 					var conditionText = rule.media.conditionText || rule.media.mediaText;
@@ -118,24 +332,49 @@ function forceUnresponsiveStyle(){
 	}
 }
 
+function saveSettings(){
+	window.localStorage['unresponsive-settings'] = JSON.stringify( settings );
+}
+
+function loadSettings(){
+	settings = window.localStorage['unresponsive-settings'];
+	if( !settings ){
+		settings = window.localStorage['responsive'];
+		settings = { responsive: settings != 'unresponsive' };
+		saveSettings();
+		window.localStorage.removeItem('responsive');
+	}
+	else{
+		settings = JSON.parse( settings );
+	}
+	return settings;
+}
+
+function hasBootstrapStyle(){
+	var bootswatchCSS = $('#bootswatchCSS');
+	if( bootswatchCSS.length ){
+		return !!bootswatchCSS[0]['href'];
+	}
+	return false;
+}
+
 function init() {
 	if(!nodebbTranslator){
 		setTimeout( init, 10 );
+		return;
 	}
-	var s = [].slice.call(document.styleSheets);
-	for(var ixs = 0; ixs < s.length; ixs++){
-		sheet = s[ixs];
-		if( sheet && sheet.href && sheet.href.indexOf('bootstrap') != -1){
-			return; // this doesn't work with skins due to cross origin issues
-		}
-	}
-	var responsive = window.localStorage['responsive'] != 'unresponsive';
-	if( !responsive ){
+	hasCustomStyle = hasBootstrapStyle();
+	loadSettings();
+	
+	if( !settings.responsive && !hasCustomStyle ){
 		forceUnresponsiveStyle();
 	}
-	addResponsiveMenu( responsive );
+	styleTweaks();
+	initInterface( hasCustomStyle );
 }
+
 $(window).on('action:widgets.loaded', function(){
 	init();
 });
 require(['translator'],  unresponsive);
+}());
